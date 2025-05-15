858958ac3e33266d87e1c5be0bc7404d0cf024ec
kailtrimming <kgtrimming@gmail.com>
2025-05-15 18:01:53 +0200

    b

--- CPower.h
+++ CPower.h
@@ -3,11 +3,7 @@
 
 #include "CFunction.h"
 
-/*#include <iostream>
-#include <cmath>
-#include <cstring>
-
-using namespace std;*/
+using namespace std;
 
 class Power : public Function
 {
@@ -17,20 +13,20 @@ class Power : public Function
 		
 	public: 
 		Power();
-		Power(const double k_coefficent, double e_coefficent);
+		Power(double k_coefficient, double e_coefficient);
 		Power(const Power& p);
 		~Power();
 		
 		Power& operator=(const Power& p);
 		bool operator==(const Power& p);
 		
-		void SetPower(const double k_coefficent, double e_coefficent);
+		void SetPower(double k_coefficient, double e_coefficient);
 		double GetValue(double in);
 		
 		void Reset();
 		
-		void ErrorMessage(const char *string); 
-		void WarningMessage(const char *string);
+		//void ErrorMessage(const char *string); 
+		//void WarningMessage(const char *string);
 		void Dump();
 };
 
